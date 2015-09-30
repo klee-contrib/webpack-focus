5fa9da085da4a6bb4e143b29499a874e725821fe
Stan <bernardstanislas@gmail.com>
2015-09-30 18:11:33 +0200

    [loaders] Add CSS loarder

--- src/dev-config.js
+++ src/dev-config.js
@@ -29,29 +29,41 @@ function generateConfiguration(spec = {}){
                     test: /\.js$/,
                     loaders: ['react-hot', 'babel'],
                     include: directory || path.join(__dirname, 'src')
-                },{
+                },
+                {
                     test: /\.json$/,
                     loaders: ['json']
-                }, {
+                },
+                {
                     test: /\.scss$/,
                     loader: 'style!css!sass'
-                },{
+                },
+                {
+                    test: /\.css$/,
+                    loader: 'style!css'
+                },
+                {
                     test: /\.png$/,
                     loader: 'url-loader',
                     query: { mimetype: 'image/png' }
-                },{
+                },
+                {
                     test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
                     loader: 'url?limit=10000&mimetype=application/font-woff'
-                },{
+                },
+                {
                     test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
                     loader: 'url?limit=10000&mimetype=application/font-woff'
-                }, {
+                },
+                {
                     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
                     loader: 'url?limit=10000&mimetype=application/octet-stream'
-                }, {
+                },
+                {
                     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
                     loader: 'file'
-                }, {
+                },
+                {
                     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
                     loader: 'url?limit=10000&mimetype=image/svg+xml'
                 },
