0ee03700603833f6c1324091ec2e240d0de45c10
Bazire Houssin <bazire.houssin@kleegroup.com>
2017-03-16 12:14:38 +0100

    Correction default conf

--- src/config/default.js
+++ src/config/default.js
@@ -14,14 +14,14 @@ const defaultConfig = (environnement, definedVariables) => {
     const BASE_URL = environnement.BASE_URL ? environnement.BASE_URL : '';
 
     // Check if focus libraries should be held locally or read from NPM
-    const localFocus = environnement.LOCAL_FOCUS ? JSON.parse(environnement.env.LOCAL_FOCUS) : false;
+    const localFocus = environnement.LOCAL_FOCUS ? JSON.parse(environnement.LOCAL_FOCUS) : false;
 
     if (localFocus) {
         config.addAlias('focus-core', path.resolve(process.cwd(), '../focus-core'));
         config.addAlias('focus-components', path.resolve(process.cwd(), '../focus-components'));
     }
 
-    config.addDefinedVariable('__API_ROOT__', JSON.stringify(process.env.API_ROOT ? process.env.API_ROOT : `${API_PROTOCOL}://${API_HOST}:${API_PORT}/${API_SUBDOMAIN}`));
+    config.addDefinedVariable('__API_ROOT__', JSON.stringify(environnement.API_ROOT ? environnement.API_ROOT : `${API_PROTOCOL}://${API_HOST}:${API_PORT}/${API_SUBDOMAIN}`));
     config.addDefinedVariable('__BASE_URL__', JSON.stringify(BASE_URL));
 }
 
