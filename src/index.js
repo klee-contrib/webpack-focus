41fd559a44b7aa4485a3e4400ee630e9f1783379
Hartorn <bazirehoussin@gmail.com>
2017-10-07 11:51:38 +0200

    Correcting launcher and config builder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { serverLauncher } from './dev-server';
 import baseConfig from './config/base';
+import envParser from './webpack-utilities/env-parser';
 
+/**
+ * Function to build a webpack conf, from a custom conf to be merged, and defined variables
+ * 
+ * @param {any} [customConf={}] a custom partial webpack conf, to be merged
+ * @param {any} [definedVariables={}] some variables to be defined
+ * @returns {object} the webpack conf built
+ */
 const configBuilder = (customConf = {}, definedVariables = {}) => {
-    return baseConfig(process.env, definedVariables).toWebpackConfig(customConf);
+    return baseConfig(process.env, definedVariables).toWebpackConfig(envParser(process.env), customConf);
 }
 
 export {
